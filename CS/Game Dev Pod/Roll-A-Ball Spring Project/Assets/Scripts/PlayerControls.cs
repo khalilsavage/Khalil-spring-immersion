b5e4a476ab20e9420b8f95fda55bf444e15a8e60
khalilsavage <khalil.savage@hgs.hiddengeniusproject.org>
2025-04-09 18:12:01 -0700

    Update PlayerControls.cs

--- CS/Game Dev Pod/Roll-A-Ball Spring Project/Assets/Scripts/PlayerControls.cs
+++ CS/Game Dev Pod/Roll-A-Ball Spring Project/Assets/Scripts/PlayerControls.cs
@@ -35,7 +35,9 @@ public class PlayerControls : MonoBehaviour
     #region Variables
     // 1. Declare a public variable of type float named 'moveSpeed'; assign it the value '5f'
     // This variable stores how fast the player character moves
-    // YOUR CODE HERE
+    public float moveSpeed = 5f;
+
+    public CharacterController controller;
 
     // 2. Declare a public variable of type CharacterController named 'controller'; Do not assign it a value here
     // This variable stores the player character's Charactercontroller component, which will be used to move the character later
@@ -43,7 +45,7 @@ public class PlayerControls : MonoBehaviour
 
     // 3. Declare a public variable of type float named 'rotateSpeed'; assign it the value '10f'
     // This variable stores how fast the player rotates when changing direction
-    // YOUR CODE HERE
+    public float rotateSpeed = 10f;
     #endregion
 
 
@@ -52,7 +54,7 @@ public class PlayerControls : MonoBehaviour
     {
         // 4. Store/Assign the player's CharacterController component to the 'controller' variable (Hint: Use the GetComponent<>() funcntion)
         // This allows us to access the player character's CharacterController component through this variable, and call functions it has like Move() 
-        // YOUR CODE HERE
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is a built-in Unity function/method called every frame of your game (there is on average 60 frames per second)
@@ -62,24 +64,30 @@ public class PlayerControls : MonoBehaviour
         // Assign this variable the output of the Input.GetAxis("") function 
         // This line of code collects the horizontal input (left & right) of the player & stores it inside the 'horizontalInput' variable
         // YOUR CODE HERE
-
+        float horizontalInput = Input.GetAxis("Horizontal");
         // 6. Declare a private variable of type float named 'vertricalInput'
         // Assign this variable the output of the Input.GetAxis("") function 
         // This line of code collects the vertical input (forward & back) of the player & stores it inside the 'verticalInput' variable
         // YOUR CODE HERE
-
+        float verticalInput = Input.GetAxis("Vertical");
         // 7. Declare a private variable of type Vector3 named 'moveDirection'
         // Assign this variable to a new Vector3(x, y, z) by plugging in your input variables for the x & z values respectively, and the y value as '0f'
         // This variable compiles all player input into a single direction the player character will move in
         // YOUR CODE HERE
-
+        Vector3 moveDirection = new Vector3(horizontalInput, 0f,verticalInput);
 
         // 8. Write an if statement with the condtion: if 'moveDirection' is NOT equal to 'Vector3.zero'
         // This checks 'if the player's move direction is not zero', and is therefore supposed to be moving 
         // (Hint: Don't forget to create {} brackets after the head of the if statement; 9 - 11 should be inside and indented within those brackets)
         // (HINT: The "does not equal" operator is !=) 
         // YOUR CODE HERE
-        
+        if (moveDirection != Vector3.zero){
+            Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotateSpeed * Time.deltaTime);
+
+            controller.Move(moveDirection * moveSpeed * Time.deltaTime);
+        }
             // 9. Declare a variable of type Quaternion named 'targetRotation'
             // Assign this variable the output of the function Quaternion.LookRotation(""), with the 'moveDirection' as the argument
             // This variable stores the desired direction we want the player character to turn towwards while moving
